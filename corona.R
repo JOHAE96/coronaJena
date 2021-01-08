49a98715e2eb93303772335315e42b3d470b78b1
johannes <hjohannnes@gmail.com>
2021-01-08 17:02:39 +0100

    more colors

--- corona.R
+++ corona.R
@@ -2,8 +2,12 @@ library(zoo)
 url <- "https://opendata.jena.de/dataset/2cc7773d-beba-43ad-9808-a420a67ffcb3/resource/d3ba07b6-fb19-451b-b902-5b18d8e8cbad/download/corona_erkrankungen_jena.csv"
 corona <- read.csv2(url, sep=",")
 
-days = 40
+#corona <- corona[!(corona$zeit == 1640991599),] # remove row with the wrong date
+
+days = 30
+
 no.rows <- nrow(corona)
+
 if (days > no.rows) {
   days = no.rows
 }
@@ -20,20 +24,31 @@ plot(df.neu[seit:no.rows,], xlab = "date", ylab = "neu", main = "neu infiziert")
 grid()
 
 
+
 df.7days <- data.frame(
   time <- as.Date(as.POSIXct(corona$zeit, origin="1970-01-01")),
   day.index <-rollsumr(corona$neu_erkrankte, k = 7, fill = NA) *100000/108127 # Einwohner Zahl 19.10.2020 laut jena.de/corona leider keine open data quelle
 )
 
 
-plot(df.7days[seit:no.rows,], xlab = "date", ylab = "7 Tage index", main = "7-Tage Index", col=ifelse(df.7days[seit:no.rows,2] > 50, "red", ifelse(df.7days[seit:no.rows,2] > 35 , "yellow", "black")), pch = ifelse(df.7days[seit:no.rows,2] > 35,19,1))
+plot(df.7days[seit:no.rows,], xlab = "date", ylab = "7 Tage index", main = "7-Tage Index", col=ifelse(df.7days[seit:no.rows,2] > 200,
+                                                                                                      "purple",
+                                                                                                      ifelse(df.7days[seit:no.rows,2] > 100,
+                                                                                                             "blue", 
+                                                                                                              ifelse(df.7days[seit:no.rows,2] > 50,
+                                                                                                                   "red", 
+                                                                                                                   ifelse(df.7days[seit:no.rows,2] > 35,
+                                                                                                                          "yellow",
+                                                                                                                          "black")))
+                                                                                                                        ), pch = ifelse(df.7days[seit:no.rows,2] > 35,19,1))
 abline(h = 35, col = 'yellow3', lwd = 1)
 abline(h = 50, col = 'coral2', lwd = 1)
+abline(h = 100, col = 'blue', lwd = 1)
+abline(h = 200, col = 'purple', lwd = 1)
 grid()
-print("Datum:")
-print(df.7days[no.rows,1])
-print("7Tage index heute:")
-print(df.7days[no.rows,2])
+
+
+
 
 
 df.neu.aktiv <- data.frame(
@@ -69,4 +84,8 @@ grid()
 
 
 
-#ifelse(day.index > 50, "red", ifelse(day.index > 35 , "yellow", "black"))
+
+print("Datum:")
+print(df.7days[no.rows,1])
+print("7Tage index heute:")
+print(df.7days[no.rows,2])
